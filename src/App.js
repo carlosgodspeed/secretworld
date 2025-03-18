556103e23231efb884c7419092138d5c0cc246e2
carlosgodspeed <carlosminibics@gmail.com>
2025-03-18 15:31:11 -0300

    desenvolvimento do projeto

--- src/App.js
+++ src/App.js
@@ -15,12 +15,40 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
 
-  const retry = () => {
-    setGameStage(stages[0].name);
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = 
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+      console.log(category);
+
+      const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+      console.log(word);
+
+      return {word, category};
   };
-  
+
+
   const startGame = () => {
+
+  const { word, category } = pickWordAndCategory();
+
+  let wordLetters = word.split("");
+
+  wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+  console.log(word, category);
+  console.log(wordLetters);
+
+  setPickedWord(word);
+  setPickedCategory(category);
+  setLetters(letters);
+  
     setGameStage(stages[1].name);
   };
 
@@ -28,6 +56,10 @@ function App() {
     setGameStage(stages[2].name);
   };
 
+  const retry = () => {
+    setGameStage(stages[0].name);
+  };
+  
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame} />}
